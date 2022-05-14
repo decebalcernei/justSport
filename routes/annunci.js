98aa7e8f06f03d2efc7ca53fb57e9ce183a83c40
decebal <decebalcernei@gmail.com>
2022-05-14 11:51:51 +0200

    aggiunta api per visualizzare tutti gli annunci

--- routes/annunci.js
+++ routes/annunci.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const Annuncio = require('../models/Annuncio'); // ci serve per interagire con il db
 
 // Get all posts
 router.get('/', async (req, res) => {
-    try{
+    try {
         const annunci = await Annuncio.find();
         res.json(annunci);
-        }catch(err){
-            res.json({message: err})
-        }
+    } catch (err) {
+        res.json({
+            message: err
+        })
+    }
 });
 
 module.exports = router;
